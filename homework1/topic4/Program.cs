025a8e53d71f7239ba9bf5cc86d619bf69062ba2
La4tgamer <32605344+La4tgamer@users.noreply.github.com>
2018-09-12 17:06:16 +0800

    homework1 update

---
+++ homework1/topic4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topic4 {
+    class Program {
+        static void Main(string[] args) {
+            int IVa1 = 0;
+            int IVa2 = 0;
+            string s;
+            Console.Write("input the first number:");
+            s = Console.ReadLine();
+            IVa1 = int.Parse(s);
+            Console.Write("input the second number:");
+            s = Console.ReadLine();
+            IVa2 = int.Parse(s);
+            Console.Write("Their product is");
+            Console.WriteLine(IVa2 * IVa2);
+        }
+    }
+}
